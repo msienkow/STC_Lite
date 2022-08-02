629eb0d14cf256d2449e5616ecf534e18c1c6a22
msienkow <msienkow@gmail.com>
2022-08-02 14:42:47 -0500

    testing

--- SaniTrend.py
+++ SaniTrend.py
@@ -1,9 +1,20 @@
+import time
 from pylogix import PLC
 import stc_lite
 
-import time
-stc_plc = stc_lite.SaniTrendPLC()
-while True:
-    if stc_plc.plc_timer():
-        print("gotcha")
-    time.sleep(.5)
+
+def main():
+    stc_lite.SaniTrendLogging.add_log_entry('warn', 'call from class')
+    # while True:
+        # test = stc_lite.SaniTrendLogging()
+        # test2 = stc_lite.Thingworx()
+        # for i in range(10):
+        #     test.add_log_entry('debug', f'Test #{i}')
+        # print(time.perf_counter())
+        # print(int(round(time.time() * 1000)))
+        # test2.get_twx_connection_status
+        # time.sleep(5)
+
+if __name__ == '__main__':
+    # asyncio.run(main())
+    main()
--- stc_lite.py
+++ stc_lite.py
@@ -1,3 +1,4 @@
+import asyncio
 from dataclasses import dataclass, field
 from datetime import datetime
 import json
@@ -7,13 +8,75 @@ import math
 import os
 import platform
 import requests
-from sqlite3 import connect
-import threading
+import sqlite3
+import sys
 import time
 
+@dataclass
+class SaniTrendLogging():
+    log_enable: bool = True
+    logger = logging.getLogger('STC_Logs')
+    logger.setLevel(logging.DEBUG)
+    logger_formatter = logging.Formatter('%(levelname)s %(asctime)s - %(funcName)s: %(message)s')
+    logger_handler = handlers.TimedRotatingFileHandler('stc.log', when='midnight', interval=1, backupCount=30)
+    logger_handler.setFormatter(logger_formatter)
+    logger.addHandler(logger_handler)
+    def add_log_entry(log_level, log_message, log_enable: bool = True) -> None:
+        if log_enable:
+            async def log(log_level, log_message) -> None:
+                level = log_level.lower()
+                log_levels = [
+                    {
+                        'log_level_logger': SaniTrendLogging.logger.debug,
+                        'search': [
+                            'de',
+                            'bug'
+                        ]
+                    },
+                    {
+                        'log_level_logger': SaniTrendLogging.logger.info,
+                        'search': [
+                            'in',
+                            'fo'
+                        ]
+                    },
+                    {
+                        'log_level_logger': SaniTrendLogging.logger.warning,
+                        'search': [
+                            'war',
+                            'arn'
+                        ]
+                    },
+                    {
+                        'log_level_logger': SaniTrendLogging.logger.error,
+                        'search': [
+                            'er',
+                            'or'
+                        ]
+                    },
+                    {
+                        'log_level_logger': SaniTrendLogging.logger.critical,
+                        'search': [
+                            'cr',
+                            'it'
+                        ]
+                    }
+                ]
+
+                log_level_to_use = SaniTrendLogging.logger.info
+                for log_dict in log_levels:
+                    for search_term in log_dict['search']:
+                        if level.find(search_term) > -1:
+                            log_level_to_use = log_dict['log_level_logger']
+                            break
+                
+                log_level_to_use(log_message)
+            
+            asyncio.run(log(log_level, log_message))
 
 
-class TimerResponse():
+
+class SimpleTimer():
     def __init__(self, entered_time: int = 0, preset: int = 0):
         self.ACC = 0
         self.DN = False
@@ -37,71 +100,6 @@ class TimerResponse():
         self.DN = True if time_elapsed >= preset else False
             
 
-
-
-class SaniTrendLogging:
-    # Set up error logger
-    stc_error_log_enable = True
-    stc_error_logger = logging.getLogger('STC_Errors')
-    stc_error_logger.setLevel(logging.WARN)
-    stc_error_logger_formater = logging.Formatter('%(asctime)s: %(message)s')
-    stc_error_logger_file_handler = handlers.TimedRotatingFileHandler('errors.log', when='midnight', interval=1, backupCount=30)
-    stc_error_logger_file_handler.setFormatter(stc_error_logger_formater)
-    stc_error_logger.addHandler(stc_error_logger_file_handler)
-
-    # Set up data logger
-    stc_data_log_enable = True
-    stc_data_logger = logging.getLogger('STC_Data')
-    stc_data_logger.setLevel(logging.INFO)
-    stc_data_logger_formater = logging.Formatter('%(asctime)s: %(message)s')
-    stc_data_logger_file_handler = handlers.TimedRotatingFileHandler('data.log', when='midnight', interval=1, backupCount=30)
-    stc_data_logger_file_handler.setFormatter(stc_data_logger_formater)
-    stc_data_logger.addHandler(stc_data_logger_file_handler)
-
-    def log_error(self, name, message) -> None:
-        """Threading wrapper to log errors using python built-in logging
-
-        Args:
-            name (str): name of function calling error log (ex. __name__)
-            message (str): error message
-        """
-        if self.stc_error_log_enable:
-            log_message = f'{name} - {message}'
-            threading.Thread(target=self._log_error(log_message)).start()
-        
-    def _log_error(self, message) -> None:
-        """Threaded execution of error log
-
-        Args:
-            message (str): error message
-        """
-        self.stc_error_logger.error(message)
-
-    def log_data(self, data: list = []) -> None:
-        """Threading wrapper to log data using python built-in logging
-
-        Args:
-            data (list, optional): data to be logged. Defaults to [].
-        """
-        if self.stc_data_log_enable:
-            message = ''
-            for i in data:
-                message = message + f'{i}, '
-            threading.Thread(target=self._log_data(self, message)).start()
-        
-    def _log_data(self, message) -> None:
-        """Threaded execution of data log
-
-        Args:
-            message (str): data to be logged
-        """
-        self.stc_data_logger.info(message)
-# Initialize the Logger
-SaniTrend_Logger = SaniTrendLogging()
-
-
-
-
 @dataclass
 class SaniTrendDatabase:
     """Class for database data logging
@@ -116,11 +114,13 @@ class SaniTrendDatabase:
 class SaniTrendPLC:
     """PLC communication class
     """
+    delta: float = 0.49
     plc_ipaddress: str = ''
     plc_path: str = ''
     plc_scan_rate: int = 1000
     tags: list = field(default_factory=list)
     tag_data: list = field(default_factory=list)
+    tag_data_buffer: list = field(default_factory=list)
     virtual_analog_input_tag: str = ''
     virtual_digital_input_tag: str = ''
     virtual_string_tag: str = ''
@@ -136,12 +136,13 @@ class SaniTrendPLC:
         Returns:
             TimerResponse: done bit, current timestamp(ms), accumulator(ms)
         """
-        timer = TimerResponse(self._plc_last_scan_time, self.plc_scan_rate)
+        timer = SimpleTimer(self._plc_last_scan_time, self.plc_scan_rate)
         if timer.DN:
             self._plc_last_scan_time = timer.timestamp
         return timer.DN
 
-
+    def set_tag_data_buffer(self) -> None:
+        pass
 
 
 @dataclass
@@ -158,51 +159,38 @@ class Thingworx:
     _twx_last_connection_test: int = 0
     _twx_conn_test_in_progress: bool = False
 
-    def get_twx_connection_status(self, preset: int = 5) -> None:
-        """Threading wrapper function to check for Thingworx connectivity.
+    def twx_conn_timer(self,) -> bool:
+        timer = SimpleTimer(self._twx_last_connection_test, 10000)
+        if timer.DN:
+            self._twx_last_connection_test = timer.timestamp
+        return timer.DN
 
-        Args:
-            preset (int, optional): Seconds to repeat check for connectivity. Defaults to 5.
-        """
-        if not self._twx_conn_test_in_progress:
-            current_ms_time = get_ms_time()
-            time_accumulated = current_ms_time - self._twx_last_connection_test
-            if time_accumulated > preset:
-                self._twx_conn_test_in_progress = True
-                self._twx_last_connection_test = current_ms_time
-                threading.Thread(target=self._get_twx_connection_status).start()
+    def get_twx_connection_status(self) -> None:
+        timer_dn = self.txw_conn_timer()
+        if timer_dn and not self._twx_conn_test_in_progress:
+            self._twx_conn_test_in_progress = True
+            async def _get_twx_connection_status(self) -> None:
+                url = 'http://localhost:8000/Thingworx/Things/LocalEms/Properties/isConnected'
+                try:
+                    connection_response = self._connection_status_session.get(url, headers=self.headers, timeout=5)
+                    if  connection_response.status_code == 200:
+                        self.twx_connected = (connection_response.json())['rows'][0]['isConnected']
 
-    def _get_twx_connection_status(self,) -> None:
-        """Threaded function for Thingworx connectivity check
-        """
-        url = 'http://localhost:8000/Thingworx/Things/LocalEms/Properties/isConnected'
-        
-        try:
-            connection_response = self._connection_status_session.get(url, headers=self.headers, timeout=30)
-            if  connection_response.status_code == 200:
-                self.twx_connected = (connection_response.json())['rows'][0]['isConnected']
+                    else:
+                        SaniTrendLogging.add_log_entry('error', connection_response)
+                        self.twx_connected = False
 
-            else:
-                SaniTrend_Logger.log_error(__name__, connection_response)
-                self.twx_connected = False
+                except Exception as e:
+                    self.twx_connected = False
+                    self._twx_last_connection_test = self._twx_last_connection_test + 30000
 
-        except Exception as e:
-            self.twx_connected = False
-            self._twx_last_connection_test = get_ms_time() + 30000
-
-        self._twx_conn_test_in_progress = False
+                self._twx_conn_test_in_progress = False
 
         
         
 @dataclass
-class SaniTrendCloud(SaniTrendDatabase, Thingworx, SaniTrendPLC):
-    """Set up initial SaniTrend™ Cloud class that will hold all the data in memory that is needed.
-    """
-    
-    # def __post_init__(self):
-    #     self.plc_path = 'test'
-    #     self.connection_headers = 
-    # twxData: list = field(default_factory=list)
+class SaniTrendCloud(SaniTrendLogging, SaniTrendPLC, Thingworx):
+    pass
     
 
 
@@ -210,7 +198,7 @@ class SaniTrendCloud(SaniTrendDatabase, Thingworx, SaniTrendPLC):
 
 def main():
     # sanitrend_logger.log_error('test', 'test123')
-    SaniTrend_Logger.log_error('test', 'test123')
+    SaniTrendLogging.add_log_entry('info', 'test123')
 
 if __name__ == '__main__':
     main()
