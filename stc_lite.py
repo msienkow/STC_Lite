fc5cbc1987d236e1009e232f17c6671239ec816d
msienkow <msienkow@gmail.com>
2022-08-12 15:14:10 -0500

    Added creation of plc to main class and sync'd the reads and writes for Micro800 comms.

--- stc_lite.py
+++ stc_lite.py
@@ -1,5 +1,4 @@
 import asyncio
-from multiprocessing.sharedctypes import Value
 import aiohttp
 from dataclasses import dataclass, field
 import json
@@ -9,7 +8,6 @@ from math import isinf
 import os
 import platform
 from pylogix import PLC, lgx_response
-import requests
 import sqlite3
 import sys
 import time
@@ -47,7 +45,6 @@ def RoundTagData(tag_data) -> any:
     """
     if isinstance(tag_data, list):
         rounded_tag_data = []
-        # Response(tag_name, value, status)
         for tag in tag_data:
             value = round(tag.Value,2) if isinstance(tag.Value, float) else tag.Value
             rounded_tag_data.append(lgx_response.Response(tag.TagName, value, tag.Status))
@@ -84,57 +81,14 @@ def get_tag_value(tag_data: list = [], tag_name: str = '') -> any:
     if tag_data and tag_name:
         values = (tag.Value for tag in tag_data if tag.TagName == tag_name)
         for value in values:
-            if isinstance(value, float):
-                return round(value, 2)
-            else:
-                return value
+            return value
 
-async def twx_request(request_type: str, url: str, headers = {}, data: dict = {}, timeout: int = 5):
-    async with aiohttp.ClientSession() as session:
-        request_types = {
-            'get': session.get,
-            'post': session.post
-        }
-        request_type = request_type.lower()
-        if request_type in request_types:
-            try:
-                async with request_types[request_type](url = url, headers = headers) as response:
-                    if response.status == 200:
-                        return await response.json()
-                    else:
-                        return None
-            except Exception as e:
-                SaniTrendLogging.logger.error(repr(e))
-        else:
-            SaniTrendLogging.logger.exception('Request method not defined.')
-
-@dataclass
-class STC:
-    smi_number: str = ''
-
-    plc_data: list = field(default_factory = list)
-    plc_data_buffer: list = field(default_factory = list)
-    plc_tag_list: list = field(default_factory = list)
-    plc_tag_delta: float = 0.495,
-    plc_ip_address: str = ''
-    plc_scan_rate: int = 1000
-    plc_last_scan_time: int = 0
-    
-    remote_plc_config: list = field(default_factory = list)
-    remote_plc_last_config_time: int = 0
- 
-    twx_tag_table: list = field(default_factory = list)
-    twx_connected: bool = False
-    twx_last_conn_test: int = 0
-    twx_conn_fail_count: int = 0
-    twx_conn_url: str = 'http://localhost:8000/Thingworx/Things/LocalEms/Properties/isConnected'
-    twx_server_url: str = ''
-    twx_headers: dict = field(default_factory = lambda: {
-        'Connection' : 'keep-alive',
+async def twx_request(request_type: str, url: str, data: dict = {}, timeout: int = 5):
+    headers = {'Connection' : 'keep-alive',
         'Accept' : 'application/json',
         'Content-Type' : 'application/json'
-    })
-    twx_data_shape: dict = field(default_factory = lambda: {
+    }
+    datashape = {
         'fieldDefinitions': {
             'name': {
                 'name': 'name',
@@ -167,18 +121,58 @@ class STC:
                 'ordinal': 0
             }
         }
-    })
+    }
+    async with aiohttp.ClientSession('http://localhost:8000') as session:
+        request_types = {
+            'get': session.get,
+            'post': session.post
+        }
+        request_type = request_type.lower()
+        if request_type in request_types:
+            try:
+                async with request_types[request_type](url, headers = headers) as response:
+                    if response.status == 200:
+                        return await response.json()
+                    else:
+                        return None
+            except Exception as e:
+                SaniTrendLogging.logger.error(repr(e))
+        else:
+            SaniTrendLogging.logger.exception('Request method not defined.')
 
+@dataclass
+class STC:
+    config_file: str = 'SaniTrendConfig.json'
+    smi_number: str = ''
+
+    plc = PLC()
+    plc_data: list = field(default_factory = list)
+    plc_data_buffer: list = field(default_factory = list)
+    plc_tag_list: list = field(default_factory = list)
+    plc_tag_delta: float = 0.5
+    plc_ip_address: str = ''
+    plc_scan_rate: int = 1000
+    plc_last_scan_time: int = 0
+
+    remote_plc_config: list = field(default_factory = list)
+    remote_plc_last_config_time: int = 0
+ 
+    twx_tag_table: list = field(default_factory = list)
+    twx_connected: bool = False
+    twx_last_conn_test: int = 0
+    twx_conn_fail_count: int = 0
+    
     def __post_init__(self) -> None:
-        with open('SaniTrendConfig.json') as file:
+        with open(self.config_file) as file:
             config_data = json.load(file)
             self.plc_ip_address = config_data['Config']['PLCIPAddress']
             self.plc_scan_rate = int(config_data['Config']['PLCScanRate'])
             self.smi_number = config_data['Config']['SMINumber']
-            self.twx_server_url = f'http://localhost:8000/Thingworx/Things/{self.smi_number}/'
             self.twx_tag_table = config_data['Tags']
             for tag in self.twx_tag_table:
                 self.plc_tag_list.append(tag['tag'])
+            self.plc.IPAddress = self.plc_ip_address
+            self.plc.Micro800 = True
             return None
     
     def plc_scan_timer(self) -> bool:
@@ -193,11 +187,43 @@ class STC:
             self.plc_last_scan_time = timer.timestamp
         return timer.done
 
+    async def ReadTags(self, tag: str = '') -> None:
+        new_data = await self.ReadTagData(tag)
+        if isinstance(new_data.Value, float):
+            new_data.Value = round(new_data.Value, 2)    
+        add_data = True
+        for old_data in self.plc_data:
+            if old_data.TagName == new_data.TagName:
+                add_data = False
+                if isinstance(new_data.Value, float):
+                    if abs(old_data.Value - new_data.Value) >= self.plc_tag_delta:
+                        old_data.Value = new_data.Value
+                else:
+                    if old_data.Value != new_data.Value:
+                        old_data.Value = new_data.Value
+        if add_data:
+            self.plc_data.append(new_data)
+        
+    async def ReadTagData(self, tags: list = []) -> lgx_response:
+        return self.plc.Read(tags)
+
+    async def WriteTags(self, tag_list):
+        try:
+            for tag in tag_list:
+                x = await self.WriteTagData(tag)
+        except Exception as e:
+            SaniTrendLogging.logger.error(repr(e))
+
+    async def WriteTagData(self, tag):
+        tag_name, tag_value = tag
+        return self.plc.Write(tag_name, tag_value)
+
     async def get_twx_connection_status(self) -> None:
         timer = SimpleTimer(self.twx_last_conn_test, 10000)
+        url = '/Thingworx/Things/LocalEms/Properties/isConnected'
         if timer.done:
             self.twx_last_conn_test = timer.timestamp
-            response = await twx_request('get', self.twx_conn_url, self.twx_headers)
+            response = await twx_request('get', url)
             if isinstance(response, dict):
                 self.twx_connected = response['rows'][0]['isConnected']
                 self.twx_conn_fail_count = 0
@@ -209,11 +235,11 @@ class STC:
                     self.twx_last_conn_test += 60000
 
     async def get_remote_plc_config(self):
-        url = f'{self.twx_server_url}Services/GetPropertyValues'
+        url = f'/Thingworx/Things/{self.smi_number}/Services/GetPropertyValues'
         timer = SimpleTimer(self.remote_plc_last_config_time, 10000)
         if timer.done and self.twx_connected:
             self.remote_plc_last_config_time = timer.timestamp
-            response = await twx_request('post', url, self.twx_headers)
+            response = await twx_request('post', url)
             if isinstance(response, dict):
                 self.remote_plc_config = []
                 result = response['rows'][0]
@@ -267,30 +293,6 @@ class STC:
                 self.remote_plc_config.append(('Virtualize_DIn', result['Virtualize_DIn']))
                 self.remote_plc_config.append(('Virtualize_String', result['Virtualize_String']))
 
-            
-
-
-    # def get_tag_data(self, tag_data: list = []) -> None:
-    #     for tag in tag_data:
-    #         add_tag = True
-    #         if isinstance(tag.Value, float):
-    #             tag.Value = round(tag.Value,2)
-    #         for tag_buffer in self.data_buffer:
-    #             if tag.TagName == tag_buffer.TagName:
-    #                 add_tag = False
-    #                 if isinstance(tag_buffer.Value, float):
-    #                     difference = abs(tag.Value - tag_buffer.Value)
-    #                     if difference > self.delta:
-    #                         tag_buffer.Value = tag.Value
-    #                 else:
-    #                     if tag.Value != tag_buffer.Value:
-    #                         tag_buffer.Value = tag.Value
-
-    #                 continue
-                    
-    #         if add_tag:
-    #             self.tag_data_buffer.append(tag)
-
 
 class SaniTrendDatabase:
     """Class for database data logging
@@ -298,86 +300,31 @@ class SaniTrendDatabase:
     database: str = os.path.join(os.path.dirname(__file__), "stc.db")
     logging_active: bool = False
 
+
+
+
+
 async def main():
+    write_counter = 0
     test = STC()
-    plc = PLC()
-    plc.IPAddress = test.plc_ip_address
-    plc.Micro800 = True
     while True:
-        if test.plc_scan_timer():
-            start = time.perf_counter()
-            plc.Write('Analog_In_Tags[0]', 'Analog_In_1')
-            plc.Write('Analog_In_Min[0]', 0)
-            plc.Write('Analog_In_Max[0]', 100)
-            plc.Write('Analog_In_Units[0]', '%')
-            plc.Write('Analog_In_Tags[1]', 'Analog_In_2')
-            plc.Write('Analog_In_Min[1]', 0)
-            plc.Write('Analog_In_Max[1]', 100)
-            plc.Write('Analog_In_Units[1]', '%')
-            plc.Write('Analog_In_Tags[2]', 'Analog_In_3')
-            plc.Write('Analog_In_Min[2]', 0)
-            plc.Write('Analog_In_Max[2]', 100)
-            plc.Write('Analog_In_Units[2]', '%')
-            plc.Write('Analog_In_Tags[3]', 'Analog_In_4')
-            plc.Write('Analog_In_Min[3]', 0)
-            plc.Write('Analog_In_Max[3]', 100)
-            plc.Write('Analog_In_Units[3]', '%')
-            plc.Write('Analog_In_Tags[4]', 'Analog_In_5')
-            plc.Write('Analog_In_Min[4]', 0)
-            plc.Write('Analog_In_Max[4]', 100)
-            plc.Write('Analog_In_Units[4]', '%')
-            plc.Write('Analog_In_Tags[5]', 'Analog_In_6')
-            plc.Write('Analog_In_Min[5]', 0)
-            plc.Write('Analog_In_Max[5]', 100)
-            plc.Write('Analog_In_Units[5]', '%')
-            plc.Write('Analog_In_Tags[6]', 'Analog_In_7')
-            plc.Write('Analog_In_Min[6]', 0)
-            plc.Write('Analog_In_Max[6]', 100)
-            plc.Write('Analog_In_Units[6]', '%')
-            plc.Write('Analog_In_Tags[7]', 'Analog_In_8')
-            plc.Write('Analog_In_Min[7]', 0)
-            plc.Write('Analog_In_Max[7]', 100)
-            plc.Write('Analog_In_Units[7]', '%')
-            plc.Write('Digital_In_Tags[0]', 'Digital_In_1')
-            plc.Write('Digital_In_Tags[1]', 'Digital_In_2')
-            plc.Write('Digital_In_Tags[2]', 'Digital_In_3')
-            plc.Write('Digital_In_Tags[3]', 'Digital_In_4')
-            plc.Write('Digital_In_Tags[4]', 'Digital_In_5')
-            plc.Write('Digital_In_Tags[5]', 'Digital_In_6')
-            plc.Write('Digital_In_Tags[6]', 'Digital_In_7')
-            plc.Write('Digital_In_Tags[7]', 'Digital_In_8')
-            plc.Write('Digital_In_Tags[8]', 'Digital_In_9')
-            plc.Write('Digital_In_Tags[9]', 'Digital_In_10')
-            plc.Write('Digital_In_Tags[10]', 'Digital_In_11')
-            plc.Write('Digital_In_Tags[11]', 'Digital_In_12')
-            plc.Write('PLC_IPAddress', '192.168.1.132')
-            plc.Write('PLC_Path', '1,0')
-            plc.Write('Virtual_AIn_Tag', '')
-            plc.Write('Virtual_DIn_Tag', '')
-            plc.Write('Virtual_String_Tag', '')
-            plc.Write('Virtualize_AIn', 2)
-            plc.Write('Virtualize_DIn', 0)
-            plc.Write('Virtualize_String', 0)
-
-            
-
-            # for tag_data in test.remote_plc_config:
-            #     (tag_name, tag_value) = tag_data
-            #     print(tag_data)
-            #     plc.Write(tag_name, tag_value)
-            end = time.perf_counter()
-            total = int((end - start) * 1000)
-            print(f'Total write time: {total}ms')
-        
         asyncio.create_task(test.get_twx_connection_status())
         asyncio.create_task(test.get_remote_plc_config())
+        if test.plc_scan_timer():
+            asyncio.create_task(test.get_twx_connection_status())
+            asyncio.create_task(test.get_remote_plc_config())
+            for tag in test.plc_tag_list:
+                asyncio.create_task(test.ReadTags(tag))
         
-        await asyncio.sleep(1)
-        # test.get_twx_connection_status()
-        # enable_plc_scan = test.plc_scan_timer()
-        # if enable_plc_scan:
-        #     print(test.twx_last_conn_test, test.twx_connected)
-        # time.sleep(0.250)
-        
+            comms = []
+            comms.append(('SaniTrend_Watchdog', get_tag_value(test.plc_data, 'PLC_Watchdog')))
+            comms.append(('Twx_Alarm', not test.twx_connected))
+            
+            asyncio.create_task(test.WriteTags(test.remote_plc_config))
+            asyncio.create_task(test.WriteTags(comms))
+               
+
+        await asyncio.sleep(5)
+
 if __name__ == "__main__":
     asyncio.run(main())
