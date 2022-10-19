63e4dd8493849af54ffbb35b9de823f4818818ac
msienkow <msienkow@gmail.com>
2022-10-19 15:03:47 -0500

    test

--- stc.py
+++ stc.py
@@ -34,7 +34,7 @@ async def main():
                 asyncio.create_task(sanitrend_cloud_lite.upload_tag_data_to_twx())
 
             
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(0.5)
 
             reboot = stc_lite.get_tag_value(sanitrend_cloud_lite.plc_data, 'Reboot')
             if reboot:
